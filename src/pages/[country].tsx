154296f6e89b352761ca7a74d267af1efb39e671
Rafael <rafa.shiga@gmail.com>
2021-06-10 10:06:01 -0300

    feat: add page title

--- src/pages/[country].tsx
+++ src/pages/[country].tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo';
 import { GetServerSideProps } from 'next';
 import Link from 'next/link';
 import { Country } from '../models/country';
@@ -17,79 +18,86 @@ export default function CountryPage({ country }: CountryProps) {
 	};
 
 	return (
-		<div className='pt-16 w-10/12 m-auto dark:text-white'>
-			<Link href='/'>
-				<a className='hover:opacity-80 transition-all duration-75 flex items-center gap-2 w-36 shadow-md py-2 px-10 dark:bg-darkBlue'>
-					<BsArrowLeft />
-					Back
-				</a>
-			</Link>
-			<div className='pt-16 flex flex-col md:flex-row gap-10 justify-between md:items-center'>
-				<img
-					className='w-full md:h-96 md:w-7/12 md:max-w-3xl object-cover'
-					src={country?.flag}
-					alt={country?.name}
-				/>
+		<>
+			<NextSeo
+				title={`Country Detail: ${country.name}`}
+				description='Simple project to show a list of countries'
+			/>
+			<div className='pt-16 w-10/12 m-auto dark:text-white'>
+				<Link href='/'>
+					<a className='hover:opacity-80 transition-all duration-75 flex items-center gap-2 w-36 shadow-md py-2 px-10 dark:bg-darkBlue'>
+						<BsArrowLeft />
+						Back
+					</a>
+				</Link>
+				<div className='pt-16 flex flex-col md:flex-row gap-10 justify-between md:items-center'>
+					<img
+						className='w-full md:h-96 md:w-7/12 md:max-w-3xl object-cover'
+						src={country?.flag}
+						alt={country?.name}
+					/>
 
-				<div>
-					<h1 className='font-bold text-3xl mb-5'>{country?.name}</h1>
-					<div className='flex flex-col md:flex-row justify-between md:gap-28'>
-						<div>
-							<p className='mb-2'>
-								<b className='font-medium'>Native name:</b> {country.nativeName}
-							</p>
-							<p className='mb-2'>
-								<b className='font-medium'>Population:</b>{' '}
-								{new Intl.NumberFormat('pt-BR').format(country.population)}
-							</p>
-							<p className='mb-2'>
-								<b className='font-medium'>Region:</b> {country.region}
-							</p>
-							<p className='mb-2'>
-								<b className='font-medium'>Sub Region:</b> {country.subregion}
-							</p>
-							<p className='mb-2'>
-								<b className='font-medium'>Capital:</b> {country.capital}
-							</p>
-						</div>
-						<div className='mt-10 md:m-0'>
-							<p className='mb-2'>
-								<b className='font-medium'>Top Level Domain:</b>{' '}
-								{country.topLevelDomain}
-							</p>
-							<p className='mb-2'>
-								<b className='font-medium'>Currencies:</b>{' '}
-								{country.currencies[0].name}
-							</p>
-							<p className='mb-2'>
-								<b className='font-medium'>Language name:</b>{' '}
-								{handleLanguages()}
-							</p>
-						</div>
-					</div>
-					{country.borders.length ? (
-						<div className='mt-10 mb-10'>
-							<div className='md:flex items-center'>
-								<b className='font-medium block mb-4 md:mb-0 mr-2'>
-									Border Countries:
-								</b>
-								<div className='flex flex-wrap gap-2'>
-									{country.borders.map((border) => (
-										<Link key={border} href={border}>
-											<a className='mr-2 py-1 px-5 font-normal rounded shadow-lg dark:bg-darkBlue hover:opacity-80'>
-												{border}
-											</a>
-										</Link>
-									))}
-								</div>
+					<div>
+						<h1 className='font-bold text-3xl mb-5'>{country?.name}</h1>
+						<div className='flex flex-col md:flex-row justify-between md:gap-28'>
+							<div>
+								<p className='mb-2'>
+									<b className='font-medium'>Native name:</b>{' '}
+									{country.nativeName}
+								</p>
+								<p className='mb-2'>
+									<b className='font-medium'>Population:</b>{' '}
+									{new Intl.NumberFormat('pt-BR').format(country.population)}
+								</p>
+								<p className='mb-2'>
+									<b className='font-medium'>Region:</b> {country.region}
+								</p>
+								<p className='mb-2'>
+									<b className='font-medium'>Sub Region:</b> {country.subregion}
+								</p>
+								<p className='mb-2'>
+									<b className='font-medium'>Capital:</b> {country.capital}
+								</p>
+							</div>
+							<div className='mt-10 md:m-0'>
+								<p className='mb-2'>
+									<b className='font-medium'>Top Level Domain:</b>{' '}
+									{country.topLevelDomain}
+								</p>
+								<p className='mb-2'>
+									<b className='font-medium'>Currencies:</b>{' '}
+									{country.currencies[0].name}
+								</p>
+								<p className='mb-2'>
+									<b className='font-medium'>Language name:</b>{' '}
+									{handleLanguages()}
+								</p>
 							</div>
 						</div>
-					) : (
-						''
-					)}
+						{country.borders.length ? (
+							<div className='mt-10 mb-10'>
+								<div className='md:flex items-center'>
+									<b className='font-medium block mb-4 md:mb-0 mr-2'>
+										Border Countries:
+									</b>
+									<div className='flex flex-wrap gap-2'>
+										{country.borders.map((border) => (
+											<Link key={border} href={border}>
+												<a className='mr-2 py-1 px-5 font-normal rounded shadow-lg dark:bg-darkBlue hover:opacity-80'>
+													{border}
+												</a>
+											</Link>
+										))}
+									</div>
+								</div>
+							</div>
+						) : (
+							''
+						)}
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import { GetServerSideProps } from 'next';
 import { useState } from 'react';
+import { NextSeo } from 'next-seo';
+import { GetServerSideProps } from 'next';
+import { BsSearch } from 'react-icons/bs';
+
 import Card from '../components/Card';
 import { Country } from '../models/country';
 import api from '../services/api';
-import { BsSearch } from 'react-icons/bs';
 
 import styles from '../styles/Home.module.css';
 
@@ -66,58 +68,64 @@ export default function Home({ countries: Countries }: HomeProps) {
 	};
 
 	return (
-		<div className='min-h-screen z-0'>
-			<div className='w-10/12 m-auto pt-10'>
-				<div className='md:flex md:justify-between md:mb-10'>
-					<div className='relative mb-10 md:mb-0'>
-						<BsSearch className='absolute top-5 left-6 dark:text-white text-gray' />
-						<input
-							type='text'
-							className='border-none rounded dark:text-white py-4 pl-16 w-full md:w-96 shadow dark:bg-darkBlue dark:placeholder-white'
-							placeholder='Search for a country...'
-							onKeyPress={handleSearch}
-							onKeyDown={resetList}
-							onChange={(event) => setSearch(event.target.value)}
-							value={search}
-						/>
-					</div>
-					<select
-						onChange={toggleRegion}
-						value={region}
-						className='p-4 mb-5 md:mb-0 shadow rounded dark:bg-darkBlue dark:text-white'
-					>
-						<option value=''>Filter by Region</option>
-						<option value='africa'>Africa</option>
-						<option value='americas'>America</option>
-						<option value='asia'>Asia</option>
-						<option value='europe'>Europe</option>
-						<option value='ocenia'>Ocenia</option>
-					</select>
-				</div>
-				{loading && (
-					<div className='flex justify-center'>
-						<div className={styles.ldRing}>
-							<div></div>
-							<div></div>
-							<div></div>
-							<div></div>
+		<>
+			<NextSeo
+				title='Countries List'
+				description='Simple project to show a list of countries'
+			/>
+			<div className='min-h-screen z-0'>
+				<div className='w-10/12 m-auto pt-10'>
+					<div className='md:flex md:justify-between md:mb-10'>
+						<div className='relative mb-10 md:mb-0'>
+							<BsSearch className='absolute top-5 left-6 dark:text-white text-gray' />
+							<input
+								type='text'
+								className='border-none rounded dark:text-white py-4 pl-16 w-full md:w-96 shadow dark:bg-darkBlue dark:placeholder-white'
+								placeholder='Search for a country...'
+								onKeyPress={handleSearch}
+								onKeyDown={resetList}
+								onChange={(event) => setSearch(event.target.value)}
+								value={search}
+							/>
 						</div>
+						<select
+							onChange={toggleRegion}
+							value={region}
+							className='p-4 mb-5 md:mb-0 shadow rounded dark:bg-darkBlue dark:text-white'
+						>
+							<option value=''>Filter by Region</option>
+							<option value='africa'>Africa</option>
+							<option value='americas'>America</option>
+							<option value='asia'>Asia</option>
+							<option value='europe'>Europe</option>
+							<option value='ocenia'>Ocenia</option>
+						</select>
 					</div>
-				)}
-				<div className='py-8 flex md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 flex-wrap gap-10 md:gap-20'>
-					{!loading &&
-						countries.map((item, index: number) => (
-							<Card country={item} key={`country-${index}`} />
-						))}
-				</div>
+					{loading && (
+						<div className='flex justify-center'>
+							<div className={styles.ldRing}>
+								<div></div>
+								<div></div>
+								<div></div>
+								<div></div>
+							</div>
+						</div>
+					)}
+					<div className='py-8 flex md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 flex-wrap gap-10 md:gap-20'>
+						{!loading &&
+							countries.map((item, index: number) => (
+								<Card country={item} key={`country-${index}`} />
+							))}
+					</div>
 
-				{!countries.length && (
-					<h3 className='dark:text-white text-4xl text-darkBlue font-bold text-center'>
-						Not Found
-					</h3>
-				)}
+					{!countries.length && (
+						<h3 className='dark:text-white text-4xl text-darkBlue font-bold text-center'>
+							Not Found
+						</h3>
+					)}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
